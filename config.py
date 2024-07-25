3047214391113178f652fd0885ee917f063c5595
ANSH883929 <hellonature70@gmail.com>
2024-07-25 11:28:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 # Don't use quotes( " and ' )
-#SCRIPT BY VENOMxCRAZY
+#SCRIPT BY V
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7376083278:AAE1K-6ob1paR5ul1ryHBbOA9mdKJio2kv4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("its_ansh_carder")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5811885547")
 
 
 
